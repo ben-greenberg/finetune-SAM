a6d829d3980a683f62306658bb729d1170c2fc51
ben <benrgreenberg@gmail.com>
2024-10-17 15:09:17 +0100

    changes to finetune SAM on OCT data

--- train_singlegpu_demo.sh
+++ train_singlegpu_demo.sh
@@ -1,32 +1,36 @@
 #!/bin/bash
 
 # Set CUDA device
-export CUDA_VISIBLE_DEVICES="5"
+export CUDA_VISIBLE_DEVICES="0"
 
 # Define variables
 arch="vit_b"  # Change this value as needed
 finetune_type="adapter"
-dataset_name="MRI-Prostate"  # Assuming you set this if it's dynamic
+dataset_name="OCT-Ear"  # Assuming you set this if it's dynamic
 targets='combine_all' # make it as binary segmentation 'multi_all' for multi cls segmentation
 # Construct train and validation image list paths
-img_folder="./datasets"  # Assuming this is the folder where images are stored
-train_img_list="${img_folder}/${dataset_name}/train_5shot.csv"
-val_img_list="${img_folder}/${dataset_name}/val_5shot.csv"
+#img_folder="/content/drive/MyDrive/to_Ben_03Sep2024"  # Assuming this is the folder where images are stored
+#train_img_list="/content/drive/MyDrive/to_Ben_03Sep2024/train_5shot.csv"
+#val_img_list="/content/drive/MyDrive/to_Ben_03Sep2024/val_5shot.csv"
+img_folder="/content/drive/MyDrive/correct_dataset/split"
+train_img_list="/content/drive/MyDrive/correct_dataset/split/train_full.csv"
+val_img_list="/content/drive/MyDrive/correct_dataset/split/val_full.csv"
 
 
 # Construct the checkpoint directory argument
-dir_checkpoint="2D-SAM_${arch}_decoder_${finetune_type}_${dataset_name}_noprompt"
+dir_checkpoint="/content/drive/MyDrive/correct_dataset/split/2D-SAM_${arch}_decoder_${finetune_type}_${dataset_name}_noprompt_Sept24"
 
 # Run the Python script
-python SingleGPU_train_finetune_noprompt.py \
+python /content/drive/MyDrive/finetuneSAMmain/SingleGPU_train_finetune_noprompt.py \
     -if_warmup True \
     -finetune_type "$finetune_type" \
     -arch "$arch" \
     -if_mask_decoder_adapter True \
     -img_folder "$img_folder" \
     -mask_folder "$img_folder" \
-    -sam_ckpt "sam_vit_b_01ec64.pth" \
+    -sam_ckpt "/content/drive/MyDrive/sam_vit_b_01ec64.pth" \
     -dataset_name "$dataset_name" \
     -dir_checkpoint "$dir_checkpoint" \
     -train_img_list "$train_img_list" \
-    -val_img_list "$val_img_list"
+    -val_img_list "$val_img_list" \
+    -num_cls 4 # n+1
